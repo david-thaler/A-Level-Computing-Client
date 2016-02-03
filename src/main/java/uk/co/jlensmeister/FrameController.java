b207b88eb5e0393a1eb02f60c96c52761c5a2fd2
JLensmeister <jlensmeister@MacBook-Air.local>
2016-02-03 11:22:09 +0000

    menu fixes

--- src/main/java/uk/co/jlensmeister/FrameController.java
+++ src/main/java/uk/co/jlensmeister/FrameController.java
@@ -4,15 +4,26 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class FrameController implements Initializable{
 
 	@FXML private Pane pane;
+	@FXML private MenuItem menuabout;
+	@FXML private MenuItem menuclose;
 	
 	private TabsController tc;
 	private TabContentController tcc;
@@ -30,6 +41,23 @@ public class FrameController implements Initializable{
 		apane.prefWidthProperty().bind(pane.widthProperty());
 		apane.prefHeightProperty().bind(pane.heightProperty());
 		pane.getChildren().add(apane);
+		
+		menuclose.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				System.exit(0);
+			}
+		});
+		
+		menuabout.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				Alert dialog = new Alert(AlertType.INFORMATION);
+				dialog.setTitle("About");
+				dialog.setGraphic(null);
+				dialog.setHeaderText(null);
+				dialog.setContentText("Version 1.0\nDeveloped by David Thaler.");
+				dialog.show();
+			}
+		});
 		
 	}
 
